4019129a525a1792619b0c9f82daafa145df8109
v-teja <tejaviswa95@gmail.com>
2023-05-22 18:22:56 +0530

    Attemp-v2

--- src/components/App.js
+++ src/components/App.js
@@ -28,7 +28,7 @@ const App = () => {
 
     console.log(exp);
     return (
-        <div className='container'>
+        <div className='container Calculator'>
             <div className='expression' >{exp}</div>
             <div className="button-container">
                 {data.map((key) => (
--- src/components/Button.js
+++ src/components/Button.js
@@ -5,9 +5,10 @@ import React from "react";
 const Button = ({id,handleClick}) => {
     
     const className = `button button-${id}`
+    cont btnId = `btn-${id}`
 
     return (
-        <button className={className} onClick={()=>handleClick(id)}>{id}</button>
+        <button className={className} id={ btnId} onClick={()=>handleClick(id)}>{id}</button>
     )
 }
 

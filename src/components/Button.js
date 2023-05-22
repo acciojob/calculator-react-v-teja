a494279dc92e15d3182675637aed178b29f12389
v-teja <tejaviswa95@gmail.com>
2023-05-22 18:32:45 +0530

    Attempt-v4

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,16 @@ import React from "react";
 const Button = ({id,handleClick}) => {
     
     const className = `button button-${id}`
-    const btnId = `btn-${id}`;
+    if (id == "/") {
+        btnId = "btn-div";
+    } else if (id == "="){
+        btnId = "equal";
+    } else if (id=="+") {
+        btnId = "plus"
+    }else{
+        const btnId = `btn-${id}`;
+    }
+   
 
     return (
         <button className={className} id={ btnId} onClick={()=>handleClick(id)}>{id}</button>
